39a3721868fc21df4cb843300e7447e18bb309d9
Brandon Anzaldi <brandon@brandonanzaldi.com>
2015-04-19 04:58:57 -0700

    Fixing error with keys

--- main.js
+++ main.js
@@ -49,6 +49,20 @@ angular.module('root', [])
     return (($scope.games[index].shots === 1) ? $scope.games[index].shots + ' shot' : $scope.games[index].shots + ' shots');
   }
   $scope.loadDefaults = function () {
-    $scope.games = JSON.parse('[{"shots":1,"count":0,"name":"Every time Nightbot times somebody out","$$hashKey":"object:6"},{"shots":1,"count":0,"name":"Every time Force mispronounces a word","$$hashKey":"object:8"},{"shots":1,"count":0,"name":"Every time something gets fucked up on OBS","$$hashKey":"object:10"},{"shots":1,"count":0,"name":"Every time TheRedCoyote professes his love for Hunter","$$hashKey":"object:12"},{"shots":2,"count":0,"name":"Every time someone comes in just to troll","$$hashKey":"object:14"},{"shots":2,"count":0,"name":"Every time Force says strem","$$hashKey":"object:16"},{"shots":2,"count":0,"name":"Every time someone follows","$$hashKey":"object:18"},{"shots":3,"count":0,"name":"Every time someone donates","$$hashKey":"object:20"},{"shots":4,"count":0,"name":"Every time 2tacopoops comes back","$$hashKey":"object:22"},{"shots":5,"count":0,"name":"If the donation is greater than $10","$$hashKey":"object:24"},{"shots":10,"count":0,"name":"If the donation is greater than $50","$$hashKey":"object:26"}]')
+    var defaults = [{"shots":1,"count":0,"name":"Every time Nightbot times somebody out","$$hashKey":"object:6"},
+    {"shots":1,"count":0,"name":"Every time Force mispronounces a word"},
+    {"shots":1,"count":0,"name":"Every time something gets fucked up on OBS"},
+    {"shots":1,"count":0,"name":"Every time TheRedCoyote professes his love for Hunter"},
+    {"shots":1,"count":0,"name":"Every time Force is disrespectful on stream"}
+    {"shots":2,"count":0,"name":"Every time someone comes in just to troll"},
+    {"shots":2,"count":0,"name":"Every time Force says strem"},
+    {"shots":2,"count":0,"name":"Every time someone follows"},
+    {"shots":3,"count":0,"name":"Every time someone donates"},
+    {"shots":4,"count":0,"name":"Every time 2tacopoops comes back"},
+    {"shots":5,"count":0,"name":"If the donation is greater than $10"},
+    {"shots":10,"count":0,"name":"If the donation is greater than $50"}];
+    defaults.forEach(function (val) {
+      $scope.games.push(val);
+    });
   }
 }]);
